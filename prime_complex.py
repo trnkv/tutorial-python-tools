3deef5a1e5206202ce0effbe919342a27a8d7236
IgorPelevanyuk <gavelock@gmail.com>
2019-09-26 16:16:48 +0300

    TEST: new set of tests prepaired

---
+++ prime_complex.py
@@ -0,0 +1,16 @@
+from prime import isPrime
+import time
+
+def test_small_0():
+    assert isPrime(0) == False
+
+def test_small_1():
+    assert isPrime(1) == False
+
+def test_small_negative():
+    assert isPrime(-7) == False
+
+def test_small_21():
+    time.sleep(2)
+    assert isPrime(21) == False
+
