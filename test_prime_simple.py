a471545b459c3be7a89b457045075ffa1809eee0
trnkv <ttrnkv75@yandex.ru>
2019-09-29 17:41:18 +0300

    Added travis

--- test_prime_simple.py
+++ test_prime_simple.py
@@ -1,13 +1,14 @@
-from Prime import isPrime
+from prime import isPrime
 import time
 
-def test_small_2():
-    time.sleep(2)
-    assert isPrime(2) == False
+def test_small_0():
+    assert isPrime(0) == False
 
-def test_small_3():
-    time.sleep(2)
-    assert isPrime(3) == True
+def test_small_1():
+    assert isPrime(1) == False
+
+def test_small_negative():
+    assert isPrime(-7) == False
 
 def test_small_21():
     time.sleep(2)
